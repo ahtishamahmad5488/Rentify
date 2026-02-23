4ba3e52ed9f5c88b56ad430bf292b9a8beaa55d0
Ahtisham Ahmad <ahtisham548849@gmail.com>
2026-02-23 22:20:46 +0500

    Foldering Done

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const server = () => {
+  return (
+    <View>
+      <Text>server</Text>
+    </View>
+  );
+};
+
+export default server;
+
+const styles = StyleSheet.create({});
